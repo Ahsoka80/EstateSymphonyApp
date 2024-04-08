ed99ff84d9c7fc81d8f7524bde9bb3bb349fa679
Nawelle Bedrouni <bedrouni.nawelle@gmail.com>
2024-04-08 13:52:06 +0200

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Hello from './componant/Hello/Hello';
+
 
 export default function App() {
   return (
+    <>
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+      <Hello />
       <StatusBar style="auto" />
     </View>
+    </>
   );
 }
 
---
+++ componant/Hello/Hello.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {Text} from 'react-native';
+
+function Hello() {
+  return (
+    <Text>Hello World ! </Text>
+  )
+}
+
+export default Hello
